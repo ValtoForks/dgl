fa88cb2dc4756b8ef82cd7b735f2de3d5fc661ec
Timur Gafarov <gecko0307@gmail.com>
2014-05-25 19:38:54 +0400

    Initial upload

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-dgl
+DGL
 ===
+DGL is a minimalistic 3D and 2D real-time graphics engine written in D language. It is a (very) thin wrapper on top of OpenGL and SDL.
 
-Graphics engine for 3D and 2D games
+Features
+--------
+* Very modest system requirements - requires OpenGL 1.2 of higher. Should work on virtually any PC
+* Supports Windows and all POSIX-compliant operating systems 
+* Object-oriented design
+* Event system
+* Layer-based rendering system. You can break up scenes to any number of 2D and 3D layers, each with it's own logics and drawable objects
+* Built-in 3D layer with trackball-style camera
+* Built-in geometric shapes (sphere, box, cylinder, cone, ellipsoid)
+* Materials and textures
+* GLSL and ARB shaders
+* Text rendering with TrueType fonts and full Unicode support (UTF-8)
+* Internationalization functionality
+
+Dependencies
+------------
+* Derelict2 (bundled)
+* dlib (http://github.com/gecko0307/dlib)
+
+License
+-------
+Copyright (c) 2013-2014 Timur Gafarov. Distributed under the Boost Software License, Version 1.0. (See accompanying file COPYING or at http://www.boost.org/LICENSE_1_0.txt)
+
---
+++ test.d
@@ -0,0 +1,76 @@
+module main;
+
+import std.stdio;
+import std.conv;
+
+import derelict.sdl.sdl;
+import derelict.opengl.gl;
+
+import dlib.math.vector;
+import dlib.image.color;
+
+import dgl.core.application;
+import dgl.core.layer;
+import dgl.ui.freeview;
+import dgl.ui.ftfont;
+import dgl.ui.textline;
+import dgl.ui.i18n;
+
+class TestApp: Application
+{
+    alias eventManager this;
+    FreeTypeFont font;
+    TextLine fpsText;
+
+    Layer layer3d;
+    Layer layer2d;
+
+    this()
+    {
+        super(640, 480, "DGL Test App");
+
+        clearColor = Color4f(0.5f, 0.5f, 0.5f);
+
+        layer3d = new FreeviewLayer(videoWidth, videoHeight);
+        addLayer(layer3d);
+
+        layer2d = addLayer2D();
+
+        font = new FreeTypeFont("data/fonts/droid/DroidSans.ttf", 27);
+
+        fpsText = new TextLine(font, localizef("FPS: %s", fps), Vector2f(10, 10));
+        fpsText.alignment = Alignment.Left;
+        fpsText.color = Color4f(1, 1, 1);
+        layer2d.addDrawable(fpsText);
+    }
+
+    override void onQuit()
+    {
+        super.onQuit();
+    }
+    
+    override void onKeyDown()
+    {
+        super.onKeyDown();
+    }
+    
+    override void onMouseButtonDown()
+    {
+        super.onMouseButtonDown();
+    }
+    
+    override void onUpdate()
+    {
+        super.onUpdate();
+
+        fpsText.setText(localizef("FPS: %s", fps));
+    }
+}
+
+void main()
+{
+    Locale.readLang("locale");
+    auto app = new TestApp();
+    app.run();
+}
+
