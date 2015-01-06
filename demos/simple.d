7d4fd027cc58889c23359a62e55a169848ffc640
unknown <gecko0307@gmail.com>
2015-01-06 13:52:19 +0300

    Added demos directory

--- demos/simple.d
+++ demos/simple.d
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014 Timur Gafarov 
+Copyright (c) 2014-2015 Timur Gafarov 
 
 Boost Software License - Version 1.0 - August 17th, 2003
 
@@ -26,76 +26,47 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-module animtest;
+module simple;
 
 import std.stdio;
+import std.conv;
 
 import derelict.sdl.sdl;
 import derelict.opengl.gl;
-import derelict.opengl.glext;
-import derelict.opengl.glu;
-import derelict.freetype.ft;
 
 import dlib.math.vector;
 import dlib.image.color;
 
 import dgl.core.application;
 import dgl.core.layer;
-import dgl.vfs.vfs;
-import dgl.vfs.file;
-import dgl.ui.i18n;
 import dgl.ui.ftfont;
 import dgl.ui.textline;
+import dgl.ui.i18n;
 import dgl.templates.freeview;
-import dgl.graphics.axes;
-import dgl.graphics.lamp;
 
-import dgl.asset.iqm;
-import dgl.scene.actor;
-
-class IQMTestApp: Application
+class TestApp: Application
 {
     alias eventManager this;
-
-    FreeviewLayer layer3d;
-    Layer layer2d;
-
-    VirtualFileSystem vfs;
-
     FreeTypeFont font;
     TextLine fpsText;
 
+    Layer layer3d;
+    Layer layer2d;
+
     this()
     {
-        super(640, 480, "IQM Demo");
+        super(640, 480, "DGL Test App");
 
         clearColor = Color4f(0.5f, 0.5f, 0.5f);
 
-        layer3d = new FreeviewLayer(videoWidth, videoHeight, 1);
-        layer3d.alignToWindow = true;
-        layer3d.drawAxes = false;
+        layer3d = new FreeviewLayer(videoWidth, videoHeight);
         addLayer(layer3d);
-        eventManager.setGlobal("camera", layer3d.camera);
 
-        layer3d.addDrawable(new Axes());
-
-        Lamp lamp = new Lamp(Vector4f(10.0f, 10.0f, 0.0f, 0.0f));
-        layer3d.addDrawable(lamp);
-
-        vfs = new VirtualFileSystem();
-        vfs.mount("data/models/mrfixit");
-
-        IQMModel model = new IQMModel(vfs.openForInput("mrfixit.iqm"), vfs);
-        Actor actor = new Actor(model); 
-        //actor.setAnimation("idle");
-        layer3d.addDrawable(actor);
-
-        layer2d = addLayer2D(-1);
-        layer2d.alignToWindow = true;
+        layer2d = addLayer2D();
 
         font = new FreeTypeFont("data/fonts/droid/DroidSans.ttf", 27);
 
-        fpsText = new TextLine(font, "", Vector2f(10, 10));
+        fpsText = new TextLine(font, localizef("FPS: %s", fps), Vector2f(10, 10));
         fpsText.alignment = Alignment.Left;
         fpsText.color = Color4f(1, 1, 1);
         layer2d.addDrawable(fpsText);
@@ -119,33 +90,15 @@ class IQMTestApp: Application
     override void onUpdate()
     {
         super.onUpdate();
+
         fpsText.setText(localizef("FPS: %s", fps));
     }
 }
 
-void loadLibraries()
-{
-    version(Windows)
-    {
-        enum sharedLibSDL = "SDL.dll";
-        enum sharedLibFT = "freetype.dll";
-    }
-    version(linux)
-    {
-        enum sharedLibSDL = "./libsdl.so";
-        enum sharedLibFT = "./libfreetype.so";
-    }
-
-    DerelictGL.load();
-    DerelictGLU.load();
-    DerelictSDL.load(sharedLibSDL);
-    DerelictFT.load(sharedLibFT);
-}
-
 void main()
 {
-    loadLibraries();
     Locale.readLang("locale");
-    auto app = new IQMTestApp();
+    auto app = new TestApp();
     app.run();
 }
+
